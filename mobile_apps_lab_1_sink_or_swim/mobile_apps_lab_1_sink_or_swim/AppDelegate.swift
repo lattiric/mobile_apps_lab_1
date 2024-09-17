f23b081bad05f427881abff26464903cdb47b9d3
alexpetmecky <petmeckyalex@gmail.com>
2024-09-17 17:11:38 -0500

    TEST COMMIT

--- mobile_apps_lab_1_sink_or_swim/mobile_apps_lab_1_sink_or_swim/AppDelegate.swift
+++ mobile_apps_lab_1_sink_or_swim/mobile_apps_lab_1_sink_or_swim/AppDelegate.swift
@@ -9,9 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-#shivani test
+//ALEX TEXT
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
