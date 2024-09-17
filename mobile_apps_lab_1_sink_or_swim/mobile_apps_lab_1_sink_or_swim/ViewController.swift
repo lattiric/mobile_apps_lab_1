a08597bc8bac01137942df97e7308c3353ab4bdc
lattiric <78038846+lattiric@users.noreply.github.com>
2024-09-16 21:13:54 -0500

    initial commit

---
+++ mobile_apps_lab_1_sink_or_swim/mobile_apps_lab_1_sink_or_swim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobile_apps_lab_1_sink_or_swim
+//
+//  Created by Rick Lattin on 9/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
